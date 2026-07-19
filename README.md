638156eaa9d7ae5af57f2ec4e4ff3aa4377f089b
Jason Huang <huangjj7@uci.edu>
2026-07-18 22:55:43 -0700

    Update README

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 First-aid kit for Makefiles. This is a Rust CLI tool to clean, heal, lint and generate structured build pipelines.
 
 ## Commands:
-- **init**: Scan directories and auto-generate Makefiles.
-- **lint**: Detect missing headers, broken paths, and syntax issues, such as when a file is deleted or renamed.
+- **lint**: Audits existing Makefile. Detect missing headers, broken paths, and syntax issues, such as when a file is deleted or renamed.
 - **heal**: Safely fix compiler warnings and prune dangling outputs. The heal subcommand deletes references to deleted files from a Makefile's dependency list, and compares missing files to newly untracked files and asks the user if they renamed it. If the user says Yes, rewrites Makefile with the updated file name.
+- **undeclared-deps**: Reports headers included in source code but not declared in Makefile.
 
 ## Setup:
 Built using Rust. To clone and run:
---
+++ src/parser.rs
@@ -0,0 +1,38 @@
+use std::collections::{HashSet, HashMap};
+use std::io::{self, BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use regex::Regex;
+
+fn extract_includes(file_path: &Path) -> io::Result<HashSet<PathBuf>> {
+    let mut includes = HashSet::new();
+
+    let mut file = File::open(path)?;
+    let mut reader = BufReader::new(file);
+
+    let re = Regex::new(r#"#include\s*["<]([^">]+)[">]"#).unwrap();
+
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = reader.read_line(&mut line)?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        if re.is_match(line) {
+            if let Some(captured) = re.captures(line) {
+                if let Some(group1) = captured.get(1) {
+                    includes.insert(group1.as_str());
+                }
+            }
+        }
+    }
+
+    Ok(includes)
+}
+
+fn find_system_library(lib_name) -> io::Result<PathBuf> {
+
+            
